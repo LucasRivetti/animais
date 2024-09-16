d547c37888248edc8c85b94d9f124a72ba635c58
Lucas Rivetti <lucassantosrivetti@gmail.com>
2024-09-16 19:24:45 -0300

    Projeto iniciado

---
+++ src/components/Banner/index.js
@@ -0,0 +1,13 @@
+
+import './Banner.css'
+
+function Banner() {
+    //parece html mas é JSX
+    return(
+        <header className="banner">
+        <img  src="/images/Header.png" alt="Banner principal da pagina"/>
+        </header>
+    ) 
+}
+
+export default Banner
