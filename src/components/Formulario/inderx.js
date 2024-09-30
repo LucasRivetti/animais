d201ad4c30424af318f4559afb564dc2d641f591
Lucas Rivetti <lucassantosrivetti@gmail.com>
2024-09-30 11:45:33 -0300

    Criando Lista suspensa

--- src/components/Formulario/inderx.js
+++ src/components/Formulario/inderx.js
@@ -1,14 +1,22 @@
 import CampoTexto from '../CampoTexto';
+import ListaSuspensa from '../ListaSuspensa';
 import './Formulario.css'
 
 const Formulario = () => {
+
+    const Habitat = [
+        'Terreste',
+        'Aquático',
+        'Aéreo'
+    ]
     return (
         <section className="formulario">
             <form>
                 <h2>Preencha os dados para criar o card do Animal</h2>
                 <CampoTexto label="Nome" placeholder="Digite o nome do animal" />
-                <CampoTexto label="Espécie" placeholder="Digite o nome da Espécie" />
+                <CampoTexto label="Espécie" placeholder="Digite a Espécie" />
                 <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
+                <ListaSuspensa label="Habitat" itens= {Habitat}/>
             </form>
         </section>
     )
